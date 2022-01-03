c56a7e86868a0fd9189c4b77237c8f5a44a3edfc
kei86 <61891585+kei86@users.noreply.github.com>
2022-01-03 13:56:00 +0900

    optional

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ import "./styles.css";
 import { Todo } from "./Todo";
 import { TodoType } from "./types/todo";
 import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+
+const user = {
+  name: "KE",
+  hobbies: ["movie", "game"]
+};
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -16,6 +22,7 @@ export default function App() {
   };
   return (
     <div className="App">
+      <UserProfile user={user} />
       <Text color="red" fontSize="32px" />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
